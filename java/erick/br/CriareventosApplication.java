f6817d3eb7f43a50cc0fb724d8048d340e090390
ErickBrunoCod <erick1991java@gmail.com>
2023-09-02 21:13:19 -0300

    O projeto esta sendo versionado pela primeira vez

---
+++ java/erick/br/CriareventosApplication.java
@@ -0,0 +1,13 @@
+package erick.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriareventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriareventosApplication.class, args);
+	}
+
+}
